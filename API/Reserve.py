4f2c3f5ae295932b3447749d95066599ce654c87
mahsa-meymari <mahsa-meymari98@gmail.com>
2018-11-16 19:09:12 +0330

    has data insertions and retrive of data

--- API/Reserve.py
+++ API/Reserve.py
@@ -20,56 +20,17 @@ class ReserveController(APIView):
             user_id = tokenizer.meta_decode(request.META)
             data = json.loads(request.body)
 
-            """
-             user = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
-    service = models.ForeignKey(to=Services, on_delete=models.DO_NOTHING, null=True)
-    sans = models.ForeignKey(Sans, on_delete=models.DO_NOTHING, null=True, blank=True)
-    description = models.TextField()
-    date = models.CharField(max_length=150)
-            """
-
             description = data['description']
             sans_id = data['sans_id']
             service_id = data['service_id']
             date = data['date']
-            
+            if (True):
+                reserve = Reserves.objects.create(user_id=user_id, description=description, sans_id=sans_id, date=date,
+                                                  service_id=service_id)
+
+            reserve_data = ReservesSerializer(reserve).data
+            return Response(reserve_data
+                            , status=status.HTTP_200_OK)
+
         except Exception :
             return Response({},status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request, format=None, *args, **kwargs):
-        try:
-            id = request.GET['service_id']
-            service=Services.objects.get(pk=id)
-            Services.objects.get().filter('')
-            service_data=ServiceSerializer(service).data
-            timetable = TimeTable.objects.get(services__id=service.id)
-            today = JalaliDate.today().__str__().replace('-','/')
-            sanses,start_of_week_date = SansController.getSansForPage(timetable_id=timetable.id,date=today)
-
-            return Response({"service":service_data,
-                             "sanses":sanses,
-                             "start_of_week_date":start_of_week_date},
-                            status= status.HTTP_200_OK)
-
-        except Exception:
-           return Response({}, status= status.HTTP_400_BAD_REQUEST)
-
-    def post(self, request, format=None, *args, **kwargs):
-        try:
-            data = json.loads(request.body)
-            date = data['date']
-            id = data['service_id']
-            service = Services.objects.get(pk=id)
-            service_data=ServiceSerializer(service).data
-            # id = request.POST['service_id']
-
-            timetable = TimeTable.objects.get(services__id=service.id)
-            (sanses,start_of_week_date) =SansController.getSansForPage(timetable_id=timetable.id,date=date)
-
-            return Response({"service": service_data,
-                             "sanses": sanses,
-                             "start_of_week_date":start_of_week_date}
-                            , status=status.HTTP_200_OK)
-        except Exception:
-             return Response({}, status= status.HTTP_400_BAD_REQUEST)
-
