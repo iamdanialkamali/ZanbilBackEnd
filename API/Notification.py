88f8d93105de358203ffae26c7549c21ace118ff
iamdanialkamali <daniel.kamali@yahoo.com>
2018-12-05 19:45:13 +0330

    sets inital smscontroller and sms data

--- API/Notification.py
+++ API/Notification.py
@@ -1,18 +1,25 @@
-import json
 from kavenegar import *
-
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 from django.core.mail import send_mail
 
 class EmailController(APIView):
-    def get(self, request, format=None, *args, **kwargs):
-        # def sendNotification(email ,reserve):
-        send_mail('subject', 'body of the message', 'notif@sandboxeb808a33cc074adb9fb924eef69c024a.mailgun.org',
-                  ['daniel.kamali@yahoo.com', 'daniel.kamali.dk@gmail.com'])
-        return Response({}, status=status.HTTP_200_OK)
+    @staticmethod
+    def sendNotification(email ,data):
+        message = "Your Resreve Has been set on\n " + data.date + "\n " +data.time
+        send_mail('Notification', message, 'notif@sandboxeb808a33cc074adb9fb924eef69c024a.mailgun.org',
+                  ['daniel.kamali@yahoo.com', email])
+        return 1
 
 
+class SMSController:
+    @staticmethod
+    def sendNotification(number,data):
+        message = "Your Resreve Has been set on\n " + data.date + "\n " +data.time
+        api = KavenegarAPI('434D634935754F355537366E526570396666524B41584A61752F426F79415944')
+        params = {
+            'sender': '100065995',
+            'receptor': number,
+            'message' : message
+        }
+        api.sms_send(params)
+        return 1
