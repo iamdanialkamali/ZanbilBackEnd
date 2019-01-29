30a467f808d99e0d07c16111b08673bee8c2d054
iamdanialkamali <daniel.kamali@yahoo.com>
2019-01-29 23:04:04 +0330

    sets Verified variable True

--- API/Reserve.py
+++ API/Reserve.py
@@ -13,7 +13,7 @@ from passlib.hash import pbkdf2_sha256  as decryptor
 class ReserveController(APIView):
     def put(self, request, format=None, *args, **kwargs):
 
-        try:
+        # try:
             user_id = tokenizer.meta_decode(request.META)
             data = json.loads(request.body)
             description = data['description']
@@ -21,11 +21,14 @@ class ReserveController(APIView):
             service_id = data['service_id']
             date = data['date']
             sans = Sans.objects.get(pk=sans_id)
-
+            verified = True
             service = Services.objects.get(pk = service_id)
             if(service.is_protected):
                 password = data['password']
                 verified = decryptor.verify(password,service.password)
+                if(not verified):
+                    return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
             reserves = Reserves.objects.filter(sans_id=sans_id,
                                             date=date,
                                             service_id=service_id,
@@ -47,5 +50,5 @@ class ReserveController(APIView):
             else:
                 raise Exception
 
-        except Exception:
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        # except Exception:
+        #     return Response({}, status=status.HTTP_400_BAD_REQUEST)
